3bc015aaa6b7331b44db4656a03403fc616af259
Nguyen Van Tuyen <88337446+nvt-10-10@users.noreply.github.com>
2023-12-28 10:16:28 +0700

    update search

--- javascrip.js
+++ javascrip.js
@@ -13,11 +13,9 @@ function generateRandomData() {
   const browsers = ["Chrome", "Firefox", "Safari", "Edge", "Internet Explorer"];
   const platforms = ["Windows", "macOS", "Linux", "Android", "iOS"];
 
-  const randomRenderingEngine =
-    renderingEngines[Math.floor(Math.random() * renderingEngines.length)];
+  const randomRenderingEngine = renderingEngines[Math.floor(Math.random() * renderingEngines.length)];
   const randomBrowser = browsers[Math.floor(Math.random() * browsers.length)];
-  const randomPlatform =
-    platforms[Math.floor(Math.random() * platforms.length)];
+  const randomPlatform = platforms[Math.floor(Math.random() * platforms.length)];
 
   const dataObject = {
     RenderingEngine: randomRenderingEngine,
@@ -36,10 +34,7 @@ function initData(entriesNumber, page, keyword, propertySort, order) {
   sortArrayByProperty(arr, propertySort, order);
   entriesNumber = arr.length > entriesNumber ? entriesNumber : arr.length;
   i = page * entriesNumber;
-  let n =
-    page * entriesNumber + entriesNumber > arr.length
-      ? arr.length
-      : i + entriesNumber;
+  let n = page * entriesNumber + entriesNumber > arr.length ? arr.length : i + entriesNumber;
   for (i; i < n; i++) {
     console.log(`${arr[i].RenderingEngine}`);
     html += `
@@ -60,19 +55,12 @@ function initPage(arr, entriesNumber, page) {
   html = "";
   for (i = 0; i < pageNumber; i++) {
     let pageIndex = i + 1;
-    html +=
-      i == page
-        ? ` <li class="page__item page__active" data-page=${i}>${pageIndex}</li>`
-        : ` <li class="page__item " data-page=${i}>${pageIndex}</li>`;
+    html += i == page ? ` <li class="page__item page__active" data-page=${i}>${pageIndex}</li>` : ` <li class="page__item " data-page=${i}>${pageIndex}</li>`;
   }
   if (arr.length > entriesNumber) {
     $(".page").css("display", "flex");
     $(".page__list").html(html);
-    $(".dataTables_info").html(
-      `Showing ${entriesNumber * page + 1} to ${
-        entriesNumber * (page + 1)
-      } of ${arr.length} entries`
-    );
+    $(".dataTables_info").html(`Showing ${entriesNumber * page + 1} to ${entriesNumber * (page + 1)} of ${arr.length} entries`);
   } else {
     $(".page").css("display", "none");
   }
@@ -83,14 +71,7 @@ function search(keyword) {
   else {
     arrSearch = [];
     arrInit.forEach((element) => {
-      if (
-        element.RenderingEngine.toLowerCase().includes(keyword) ||
-        element.Browser.toLowerCase().includes(keyword) ||
-        element.Platform.toLowerCase().includes(keyword) ||
-        element.EngineVersion.toLowerCase().includes(keyword) ||
-        element.CssGrade.toLowerCase().includes(keyword)
-      )
-        arrSearch.push(element);
+      if (["RenderingEngine", "Browser", "Platform", "EngineVersion", "CssGrade"].some((property) => element[property].toLowerCase().includes(keyword))) arrSearch.push(element);
     });
   }
   return arrSearch;
@@ -143,43 +124,31 @@ $(".page__list").on("click", ".page__item", function () {
 $(".table").on("click", ".table-title", function () {
   let sort = $(this).data("sort");
   let arrSort = sort.split("__");
-  let newSort = sort.endsWith("__ASC")
-    ? sort.replace("__ASC", "__DESC")
-    : sort.replace("__DESC", "__ASC");
+  let newSort = sort.endsWith("__ASC") ? sort.replace("__ASC", "__DESC") : sort.replace("__DESC", "__ASC");
   propertySort = arrSort[0];
   order = arrSort[1];
   console.log(propertySort + "\t" + order);
   initData(entriesNumber, 0, keyword, propertySort, order);
   $(this).data("sort", newSort);
   $(".table-title").removeClass("reverse");
-  $(this).hasClass("reverse")
-    ? $(this).removeClass("reverse")
-    : $(this).addClass("reverse");
+  $(this).hasClass("reverse") ? $(this).removeClass("reverse") : $(this).addClass("reverse");
 });
 
 $(".prev").on("click", function (event) {
   page = parseInt($(".page__active").data("page"));
 
-  page - 1 < 0
-    ? event.stopPropagation()
-    : initData(entriesNumber, --page, keyword, propertySort, order);
+  page - 1 < 0 ? event.stopPropagation() : initData(entriesNumber, --page, keyword, propertySort, order);
 });
 
 $(".next").on("click", function () {
   page = parseInt($(".page__active").data("page"));
-  page + 1 == pageNumber
-    ? event.stopPropagation()
-    : initData(entriesNumber, ++page, keyword, propertySort, order);
+  page + 1 == pageNumber ? event.stopPropagation() : initData(entriesNumber, ++page, keyword, propertySort, order);
 });
 
 $(".next").hover(function () {
-  parseInt($(".page__active").data("page")) + 1 == pageNumber
-    ? $(this).css("cursor", "no-drop")
-    : $(this).css("cursor", "pointer");
+  parseInt($(".page__active").data("page")) + 1 == pageNumber ? $(this).css("cursor", "no-drop") : $(this).css("cursor", "pointer");
 });
 
 $(".prev").hover(function () {
-  parseInt($(".page__active").data("page")) == 0
-    ? $(this).css("cursor", "no-drop")
-    : $(this).css("cursor", "pointer");
+  parseInt($(".page__active").data("page")) == 0 ? $(this).css("cursor", "no-drop") : $(this).css("cursor", "pointer");
 });
